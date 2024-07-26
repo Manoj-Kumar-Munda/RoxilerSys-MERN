1fbd9001505e926831be0b1c8ebd678e770e2346
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-07-26 19:00:50 +0530

    initDb controller created to initialize data

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,21 @@
 import express from "express";
 import cors from "cors";
+import productRouter from "./routers/product.router.js";
 
 const app = express();
 
 app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
+app.use("/api/v1/products", productRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
 export default app;
