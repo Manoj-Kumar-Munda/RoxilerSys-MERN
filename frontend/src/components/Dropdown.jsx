8b5f8d363587f6d2efa99786620a6182a7df9438
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-07-28 23:50:52 +0530

    task finished

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -4,7 +4,11 @@ import { monthsArr } from "../utils/constants";
 const Dropdown = ({ month, setMonth }) => {
   return (
     <div>
-      <select className="text-sm border-2 py-2 px-4 rounded-md border-gray-500" value={month} onChange={(e) => setMonth(e.target.value)}>
+      <select
+        className="text-sm border-2 py-2 px-4 rounded-md border-gray-500"
+        value={month}
+        onChange={(e) => setMonth(e.target.value)}
+      >
         {monthsArr.map((item) => (
           <option key={item.value} value={item.value}>
             {item.name}
